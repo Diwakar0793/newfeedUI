498812ca0797cf3afcd458fd682566e51b34dbd4
Diwakar0793 <54579283+Diwakar0793@users.noreply.github.com>
2025-03-11 15:33:37 +0530

    ui

--- server.ts
+++ server.ts
@@ -17,7 +17,7 @@ export function app(): express.Express {
 
   // Enable CORS for all routes with a more permissive configuration for development
   server.use(cors({
-    origin: 'http://localhost:4200',
+    origin: 'https://newsfeed-api-six.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
